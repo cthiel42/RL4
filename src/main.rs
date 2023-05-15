6d9645649564fba56ff84a5c78b854f9512b5080
Chad Thielen <48074114+cthiel42@users.noreply.github.com>
2023-05-15 03:44:33 +0000

    Hello world works
    Docs are still half baked notes. I will push those up within the next few days

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-#![no_std]
+#![no_std] 
 #![no_main]
 
 use core::panic::PanicInfo;
+static HELLO: &[u8] = b"Hello World!";
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
 
-bootloader_api::entry_point!(kernel_main);
-
-#[no_mangle]
-pub fn kernel_main(_boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
