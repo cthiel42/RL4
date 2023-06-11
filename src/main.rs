1c85861d14e80a222c28a14342e3b338b4e204eb
Chad Thielen <48074114+cthiel42@users.noreply.github.com>
2023-06-11 00:08:51 +0000

    Switch to elf crate
    Moved some things around. Think the idea now is to load the ELF into the page table and then fill in the rest of the table with unused memory. Then set a stack pointer and return everything. I think from there I should have a lot of the memory work for running an ELF done.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 
 use core::panic::PanicInfo;
 use bootloader::{BootInfo};
-use x86_64::{structures::paging::{Page, PhysFrame, OffsetPageTable}, VirtAddr, PhysAddr};
+use x86_64::{structures::paging::{Page, PhysFrame, OffsetPageTable, Size4KiB, PageSize}, VirtAddr, PhysAddr};
 use memory::BootInfoFrameAllocator;
 
 #[macro_use]
@@ -34,42 +34,62 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-fn root_thread_init_memory(boot_info: &'static BootInfo) -> (OffsetPageTable, memory::BootInfoFrameAllocator, VirtAddr) {
+fn root_thread_init_memory(boot_info: &'static BootInfo) -> (OffsetPageTable, memory::BootInfoFrameAllocator, VirtAddr, VirtAddr) {
     use bootloader::bootinfo::MemoryRegionType;
+    use elf::endian::AnyEndian;
+    use elf::abi::PT_LOAD;
+    use elf::ElfBytes;
+    use elf::segment::ProgramHeader;
+
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
 
-    // find all unused memory regions
-    let unused_regions = boot_info.memory_map.iter().filter(|r| r.region_type == MemoryRegionType::Usable);
+    // retrieve elf binary from contents of elf_data.rs
+    let file = ElfBytes::<AnyEndian>::minimal_parse(ELF_DATA).unwrap();
+    let common_sections = file.find_common_data().unwrap();
+    let first_load_phdr: Option<ProgramHeader> = file.segments().unwrap()
+        .iter()
+        .find(|phdr|{phdr.p_type == PT_LOAD});
 
-    // map each unused region to a page in the new page table and keep track of highest address for stack pointer
-    let mut page_counter = 0;
-    for region in unused_regions {
-        println!("Unused Region: {:?}", region);
-        let start_frame = PhysFrame::containing_address(PhysAddr::new(region.range.start_addr()));
-        let end_frame = PhysFrame::containing_address(PhysAddr::new(region.range.end_addr()));
-        for frame in PhysFrame::range_inclusive(start_frame, end_frame) {
-            // println!("Frame: {:?}", frame);
-            let page = Page::containing_address(VirtAddr::new(page_counter));
-            page_counter += 4096;
-            // println!("Page: {:?}", page);
-            memory::create_mapping(page, frame, &mut mapper, &mut frame_allocator);
+    // create entrypoint
+    let entry_point: u64 = first_load_phdr.unwrap().p_vaddr;
+    let entry_page: u64 = entry_point / Size4KiB::SIZE as u64;
+    println!("Entry point is at: {}", entry_point);
+    println!("Entry page is at: {}", entry_page);
+
+    // map the elf into a page table
+    for program_header in file.segments().unwrap().iter() {
+        let segment_virtual_address: u64 = program_header.p_vaddr; // virtual address the segment should be loaded
+        let segment_physical_address: u64 = program_header.p_paddr; // physical address the segment should be loaded
+        let segment_size: u64 = program_header.p_memsz; // size of the segment in bytes
+        println!("Segment virtual address: {}", segment_virtual_address);
+        println!("Segment physical address: {}", segment_physical_address);
+
+        // Calculate the number of pages needed to load the segment
+        let num_pages = (segment_size + Size4KiB::SIZE - 1) / Size4KiB::SIZE;
+
+        // Iterate over the pages in the segment
+        for page_offset in 0..num_pages {
+            let page_virtual_address = segment_virtual_address + page_offset * Size4KiB::SIZE as u64;
+
+            // Get the physical frame for the page
+            let frame = PhysFrame::<Size4KiB>::containing_address(PhysAddr::new(segment_physical_address + page_offset * Size4KiB::SIZE as u64));
+
+            // Map the virtual address to the physical frame in the page table
+            memory::create_mapping(Page::<Size4KiB>::containing_address(VirtAddr::new(page_virtual_address)), frame, &mut mapper, &mut frame_allocator);
         }
     }
 
-    // set stack pointer to highest address in page table
-    let stack_pointer = VirtAddr::new(page_counter-4096);
+    // TODO: fill in the rest of the memory with unused pages
+    let unused_regions = boot_info.memory_map.iter().filter(|r| r.region_type == MemoryRegionType::Usable);
 
-    // TODO: load root binary or ELF into memory
-    // retrieve elf binary from contents of elf_data.rs
+    // TODO: set stack pointer to highest address in page table
+    let stack_pointer = VirtAddr::new(0);
 
-
-    // TODO: assign instruction pointer? Possibly from ELF header
-
-    (mapper, frame_allocator, stack_pointer)
+    (mapper, frame_allocator, stack_pointer, VirtAddr::new(entry_point))
 }
 
 fn test_memory(boot_info: &'static BootInfo) {
